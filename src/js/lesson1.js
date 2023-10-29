ca222e5b6fd1fbe4a12f6d70424b73403393eddf
wellastra~ <you@example.com>
2023-10-29 13:06:57 +0200

    hw

---
+++ src/js/lesson1.js
@@ -0,0 +1,13 @@
+
+
+let name = 'gora'
+
+let number = 42
+
+let city = 'Rivne' 
+
+console.log (name)
+console.log (number)
+console.log (city)
+
+number = 55
---
+++ src/js/lesson2.js
@@ -0,0 +1,10 @@
+function min(a, b) {
+    if (a < b) {
+        return a;
+    } else {
+        return b;
+    }
+}
+
+console.log(1, 6);
+
